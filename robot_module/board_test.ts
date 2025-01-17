7b6d877e66df155745f8ae606119ec5c2364f9b3
im28 <ibrahim.elshamandi@gmail.com>
2025-01-17 12:25:24 +0800

    add find path to board

--- robot_module/board_test.ts
+++ robot_module/board_test.ts
@@ -8,7 +8,7 @@ Deno.test("BasicBoard", async (t) => {
       const board = new BasicBoard();
       const dimensions = board.getDimensions();
       assertEquals(dimensions, { height: 5, width: 5 });
-    },
+    }
   );
 
   await t.step(
@@ -17,7 +17,7 @@ Deno.test("BasicBoard", async (t) => {
       const board = new BasicBoard(10, 10);
       const dimensions = board.getDimensions();
       assertEquals(dimensions, { height: 10, width: 10 });
-    },
+    }
   );
 
   await t.step("canPlace returns true for valid positions", () => {
@@ -68,6 +68,77 @@ Deno.test("BasicBoard", async (t) => {
       for (const position of invalidPositions) {
         assertEquals(board.canPlace(position), false);
       }
-    },
+    }
   );
+
+  await t.step("findPath can find a path from start to goal", () => {
+    /*/
+    0 1 G 1 0
+    0 1 0 1 0
+    0 1 0 1 0
+    0 1 0 1 0
+    0 1 X 1 0
+    */
+
+    const board = new BasicBoard(5, 5);
+    board.addObstacle({ x: 1, y: 0 });
+    board.addObstacle({ x: 1, y: 1 });
+    board.addObstacle({ x: 1, y: 2 });
+    board.addObstacle({ x: 1, y: 3 });
+    board.addObstacle({ x: 1, y: 4 });
+    board.addObstacle({ x: 3, y: 0 });
+    board.addObstacle({ x: 3, y: 1 });
+    board.addObstacle({ x: 3, y: 2 });
+    board.addObstacle({ x: 3, y: 3 });
+    board.addObstacle({ x: 3, y: 4 });
+
+    const path = board.findPath({ x: 2, y: 0 }, { x: 2, y: 4 });
+    assertEquals(path, [
+      { x: 2, y: 1 },
+      { x: 2, y: 2 },
+      { x: 2, y: 3 },
+      { x: 2, y: 4 },
+    ]);
+  });
+
+  await t.step("findPath cannot find a path when it is blocked by obstacles", () => {
+    /*/
+    0 0 0 0 G
+    0 0 0 0 0
+    0 0 0 0 0
+    1 1 0 0 0
+    X 1 0 0 0
+    */
+
+    const board = new BasicBoard(5, 5);
+    board.addObstacle({ x: 1, y: 1 });
+    board.addObstacle({ x: 1, y: 0 });
+    board.addObstacle({ x: 0, y: 1 });
+    const path = board.findPath({ x: 0, y: 0 }, { x: 4, y: 4 });
+    assertEquals(path, []);
+  });
+
+  await t.step("findPath cannot find a path when it is blocked by obstacles", () => {
+    /*/
+    0 0 0 0 0
+    X 1 0 0 0
+    0 1 0 0 0
+    0 0 0 0 0
+    0 0 0 0 G
+    */
+
+    const board = new BasicBoard(5, 5);
+    board.addObstacle({ x: 1, y: 2 });
+    board.addObstacle({ x: 1, y: 3 });
+    const path = board.findPath({ x: 0, y: 3 }, { x: 4, y: 0 });
+    assertEquals(path, [
+      { x: 0, y: 2 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+      { x: 3, y: 1 },
+      { x: 4, y: 1 },
+      { x: 4, y: 0 },
+    ]);
+  });
 });
